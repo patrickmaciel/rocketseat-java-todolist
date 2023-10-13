df6ba8a5bb0ec86fc7bbc1a399b896b448bf1b03
Patrick Maciel Campos <patrickmaciel.info@gmail.com>
2023-10-13 20:42:11 -0300

    :sparkles: defining getters and setters for UserModel

--- src/main/java/br/com/rocketseat/todolist/user/UserModel.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserModel.java
@@ -1,7 +1,31 @@
 package br.com.rocketseat.todolist.user;
 
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    public void setusername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
 }
